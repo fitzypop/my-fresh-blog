5c4fb6b68301d3fc47546af3bade7b43f51e19c7
fitzypop <32967490+fitzypop@users.noreply.github.com>
2024-04-26 23:10:04 -0600

    update linkedin profile url

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,7 @@ export const settings = {
   copyrightName: "Fitzypop's Blog",
   description: "A python dev's blog, written with Deno and Fresh.",
   githubUrl: "https://github.com/fitzypop/my-fresh-blog",
-  linkedinUrl: "https://www.linkedin.com/in/joseph-fitzgibbons-5930341aa",
+  linkedinUrl: "https://www.linkedin.com/in/joe-fitzgibbons",
   ogImage: "https://fitzypop.deno.dev/og-image.png",
   postsPath: "./content/posts",
   title: "Fitzypop's Blog",
