5b279f1e7ff9b03f27b985b88a6742a24e8fb5ed
Joe Fitzgibbons <32967490+joe-c00l-fitzy@users.noreply.github.com>
2024-10-06 16:28:55 -0600

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,9 @@
 export const settings = {
   copyrightName: "Fitzypop's Blog",
   description: "A python dev's blog, written with Deno and Fresh.",
-  githubUrl: "https://github.com/fitzypop/my-fresh-blog",
+  githubUrl: "https://github.com/joe-c00l-fitzy/blog",
   linkedinUrl: "https://www.linkedin.com/in/joe-fitzgibbons",
-  ogImage: "https://fitzypop.deno.dev/og-image.png",
+  ogImage: "https://jfitzy.deno.dev/og-image.png",
   postsPath: "./content/posts",
-  title: "Fitzypop's Blog",
+  title: "Joe Fitzy's Blog",
 };
